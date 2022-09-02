3281bde9d4bd95612d778f3b5d0c5bfaa393bb33
Daniil <dan.dad@bk.ru>
2022-09-02 16:42:07 +0300

    final

--- dishes/dishes/api/urls.py
+++ dishes/dishes/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from .views import FoodCategoryView
 
 router = DefaultRouter()
-
+router.register('foods', FoodCategoryView, basename='foods')
 
 urlpatterns = [
     path('v1/', include(router.urls))
--- dishes/dishes/api/views.py
+++ dishes/dishes/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Food, FoodCategory
+from .serializers import FoodSerializer, FoodListSerializer
+from django.db.models import Prefetch
 
-# Create your views here.
+
+class FoodCategoryView(viewsets.ViewSet):
+    
+    serializer_class = FoodListSerializer
+
+    def list(self, request):
+        queryset = (
+            FoodCategory
+            .objects
+            .prefetch_related(
+                Prefetch(
+                    'food',
+                    queryset=Food.objects.filter(is_publish=True)))
+            .order_by('pk'))
+
+        serializer = FoodListSerializer(queryset, many=True)
+        return Response(serializer.data)
